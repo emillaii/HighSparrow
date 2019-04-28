bd6afb239b6fd799de28c0db7a1f7213b72f19c9
MaoLinWang <1271898687@qq.com>
2019-04-28 22:17:53 +0800

    debug

--- sensorpickarm.cpp
+++ sensorpickarm.cpp
@@ -26,3 +26,30 @@ bool SensorPickArm::move_XY_Synic(QPointF position, bool check_softlanding, int 
     resut &= motor_y->WaitArrivedTargetPos(position.y(),timeout);
     return resut;
 }
+
+bool SensorPickArm::stepMove_XYT1_Synic(const PrOffset offset, const bool check_softlanding, int timeout)
+{
+    if(check_softlanding)
+    {
+        if(!picker1->motor_z->resetSoftLanding(timeout))return false;
+        if(!picker2->motor_z->resetSoftLanding(timeout))return false;
+    }
+
+    motor_x->StepMove(offset.X);
+    motor_y->StepMove(offset.Y);
+    picker1->motor_t->StepMove(offset.Theta);
+    bool resut = motor_x->WaitArrivedTargetPos(offset.X,timeout);
+    resut &= motor_x->WaitArrivedTargetPos(offset.X,timeout);
+    resut &= picker1->motor_t->WaitArrivedTargetPos(offset.Theta,timeout);
+    return resut;
+}
+
+bool SensorPickArm::stepMove_XYT2_Synic(const PrOffset offset, const bool check_softlanding, int timeout)
+{
+    return true;
+}
+
+bool SensorPickArm::move_XYT_Synic(const PrOffset offset, const bool check_softlanding, int timeout)
+{
+    return true;
+}
--- sensorpickarm.h
+++ sensorpickarm.h
@@ -12,8 +12,9 @@ public:
     SensorPickArm(QString name = "SensorPickArm");
     void Init(XtMotor* motor_x,XtMotor* motor_y,MaterialPicker * picker1,MaterialPicker * picker2);
     bool move_XY_Synic(const QPointF position,const bool check_softlanding = false,int timeout = 10000);
-    bool stepMove_XYT1_Synic(const PrOffset offset);
-    bool move_XYT_Synic(const PrOffset offset);
+    bool stepMove_XYT1_Synic(const PrOffset offset,const bool check_softlanding = false,int timeout = 10000);
+    bool stepMove_XYT2_Synic(const PrOffset offset,const bool check_softlanding = false,int timeout = 10000);
+    bool move_XYT_Synic(const PrOffset offset,const bool check_softlanding = false,int timeout = 10000);
 private:
     XtMotor* motor_x = Q_NULLPTR;
     XtMotor* motor_y = Q_NULLPTR;

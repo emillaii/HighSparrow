de9c0b8cd00aa89f73548b8363fffcfa57e7f617
emillaii <laikwoktai@gmail.com>
2019-05-05 01:18:57 +0800

    Add least square fit circle.

--- commonutils.h
+++ commonutils.h
@@ -3,6 +3,7 @@
 
 #include <QString>
 #include <QJsonObject>
+#include <opencv/cv.h>
 
 void createAllDir();
 QString getCurrentTimeString();
@@ -15,5 +16,6 @@ QString getDir(QString);
 void writeFile(QString data, QString dir, QString filename);
 QJsonObject getJsonObjectFromString(const QString jsonString);
 QString getStringFromJsonObject(const QJsonObject& jsonObject);
+bool fitCircle(const std::vector<cv::Point2d> &pnts, cv::Point2d &center, double &radius);
 
 #endif // COMMONUTILS_H

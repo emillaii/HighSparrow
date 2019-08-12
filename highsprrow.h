9968498c5c172631b80c9d74ee7710a8e3567850
tong668 <52744724+tong668@users.noreply.github.com>
2019-08-12 21:35:59 +0800

    fix some small bug

--- highsprrow.h
+++ highsprrow.h
@@ -15,16 +15,32 @@ class HighSprrow : public QObject
 public:
     HighSprrow();
     ~HighSprrow();
-
+private:
+    ErrorCodeStruct InitComponent();
 public:
     WorkersManager* worker_manager;
     BaseModuleManager * baseModuleManager = Q_NULLPTR;
     LogicManager * logicManager = Q_NULLPTR;
+public:
+
+    Q_INVOKABLE void performLUTMushroomPR();
+    Q_INVOKABLE void performSUTDownlookPR();
+    Q_INVOKABLE void performOnlySUTDownlookPR();
+    Q_INVOKABLE void performLUTUplookPR();
+
+    Q_INVOKABLE void performLPALensPR();
+    Q_INVOKABLE void performLUTLensPR();
+    Q_INVOKABLE void performVacancyLensPR();
+
+    Q_INVOKABLE void callQProcess(QString arg);
+    Q_INVOKABLE void aa_A_Inter(double step);
+    Q_INVOKABLE void aa_B_Inter(double step);
 
 signals:
     void displaySfrImageInUI();
     void displayOCImageInUI();
 public slots:
+    void homeAllMotors();
     Q_INVOKABLE void receiveImageFromAACore(int type) {
         qInfo("Display SFR image in UI: %d", type);
         if (type == 0) emit displaySfrImageInUI();

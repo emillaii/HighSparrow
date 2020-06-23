9b3abb119857a35c95d63eb30a0274c275a54180
EdwardZhou0311 <43225626+EdwardZhou0311@users.noreply.github.com>
2020-06-23 11:05:45 +0800

    Add batch to kill mini server and exe in thread, and start new.

---
+++ Kill&Start.bat
@@ -0,0 +1,4 @@
+taskkill /f /im HighSprrowQ.exe
+taskkill /f /im miniweb.exe
+start /min C:\HighSparrowQ\flowchart\miniweb.exe -r C:\HighSparrowQ\flowchart
+start /d "C:\HighSparrowQ" HighSprrowQ.exe

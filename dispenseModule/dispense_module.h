137b618d6657edf12b5f0c3c1a3281be8b7ffc10
emillaii <laikwoktai@gmail.com>
2020-08-01 22:48:29 +0800

    Add dispense circle path editor and config file parser function (merge from trunk)

--- dispenseModule/dispense_module.h
+++ dispenseModule/dispense_module.h
@@ -22,6 +22,7 @@ public:
     Q_INVOKABLE void moveToDispenseDot(bool record_z = true);
     Q_INVOKABLE void calulateOffset(int digit = 4);
     Q_INVOKABLE bool performDispense();
+    bool getDispenseCircleProperties(double &center_x_in_mm, double &center_y_in_mm);
     Dispenser* dispenser = Q_NULLPTR;
 private:
     QVector<mPoint3D> getDispensePath();
